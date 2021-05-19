4954d2d339c9d51f373d486206978974db73ab2e
Hanen <hanen.benhassine@esprit.tn>
2021-05-19 18:58:27 +0200

    Initial commit

---
+++ ProjetFilm/src/main/java/Projet/com/ProjetFilm/ProjetFilmApplication.java
@@ -0,0 +1,13 @@
+package Projet.com.ProjetFilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFilmApplication.class, args);
+	}
+
+}
---
+++ ProjetFilm/src/test/java/Projet/com/ProjetFilm/ProjetFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package Projet.com.ProjetFilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
